ce16591d74f8a1050df18a1e122e8e5350bf9a83
Ehsaan Mondal <mail.ehsaan@gmail.com>
2023-06-03 00:59:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,22 @@
-const express = require('express')
-const app = express()
-app.all('/', (req, res) => {
-    console.log("Just got a request!")
-    res.send('Yo!')
-})
-app.listen(process.env.PORT || 3000)
+const express = require('express');
+const app = express();
+
+// Define the '/booking/api' route
+app.all('/booking/api', (req, res) => {
+  console.log("Just got a request!");
+
+  // JSON data to be sent as a response
+  const jsonData = {
+    bookingId: 1,
+    name: 'John Doe',
+    date: '2023-06-02'
+  };
+
+  // Send the JSON data as a response
+  res.json(jsonData);
+});
+
+// Start the server
+app.listen(process.env.PORT || 3000, () => {
+  console.log('Server is running on http://localhost:3000');
+});
